0ab2423c4a25c3ece0193a460c74e38008f29fc4
Lécia Tamara <leciatamara60@gmail.com>
2025-07-05 18:04:55 -0300

    formulário de adicionar administrador 80% estilizado

--- clinica/administrador/forms.py
+++ clinica/administrador/forms.py
@@ -9,5 +9,18 @@ class AdministradorForm(UserCreationForm):
         model = Administrador
         fields = ['nome', 'username']
         widgets = {
-            'nome' : forms.TextInput(attrs={'class':'form-control', 'placeholder':'nome'})
+            'nome': forms.TextInput(attrs={
+                'class': 'form-control py-3 border-white bg-transparent text-white w-50','placeholder': 'Nome'}),
+            
+            'username': forms.TextInput(attrs={
+                'class': 'form-control py-3 border-white bg-transparent text-white w-50', 'placeholder': 'Usuário'}),
         }
+    
+    #Adiciona estilos nos campos da senha
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control py-3 border-white bg-transparent text-white w-50','placeholder': 'Senha'})
+       
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control py-3 border-white bg-transparent text-white w-50','placeholder': 'Confirme a senha'})
